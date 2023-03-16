b7176ad58fe5b82d7f65c820c7eea18db34b529f
Anaclaraduartem <anaclaraduartemiranda03@gmail.com>
2023-03-16 09:02:11 -0300

    feito tab2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,6 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+ // declarar as variaveis
+
+botao1 = 1;
+botao2 = 3;
+botao3 = 6;
+botao4 = 9;
+botao5 = 12;
+
+
 
   constructor() {}
 
